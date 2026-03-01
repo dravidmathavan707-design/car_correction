7b1ad340a6e7d6d533ca1b41929df1535a60f2c1
dravidmathavan707 <dravidmathavan707@gmail.com>
2026-03-01 13:53:10 +0530

    Use environment variables in config for cloud deployment

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-SECRET_KEY = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0"
-DATABASE_NAME = "car_management"
-MONGO_URI = "mongodb://localhost:27017/"
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "admin123"
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY", "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "car_management")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin123")
